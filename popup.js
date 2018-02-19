00bca47b5ae195a12c9dc5abe4a828fa9efa6140
Tyler Kuhn <tyler@wargoats.com>
2018-02-18 19:02:15 -0800

    added popup ui

---
+++ popup.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function checkContract() {
+  let contractAddress = document.getElementById('contractAddress').value;
+  let results = document.getElementById("results");
+  results.innerHTML = "Analyzing";
+
+  let url = `https://mythril-web.herokuapp.com/infura/mainnet/${contractAddress}`;
+
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      results.innerHTML = '';
+      if (xhr.status < 400) {
+        results.innerHTML = JSON.parse(xhr.responseText).body;
+        clearInterval(loader);
+        // TODO response appropriately to errors returned with 200
+      } else {
+        results.innerHTML = 'Error saving: ' + xhr.statusText;
+      }
+    }
+  };
+
+  xhr.send();
+
+  let loader = setInterval(function() {
+    results.innerHTML += ".";
+  }, 500);
+
+  // TODO set badge to the number of vulnerabilities found
+  //chrome.browserAction.setBadgeText({text: 'ON'});
+}
+
+document.getElementById('checkContract').addEventListener('click', checkContract);
